5685da8348ee8ae1cb35a644bdf3febb9f0346a8
Sanzhar <ssoltybekov@gmail.com>
2025-10-17 20:59:15 +0500

    db connection db.go

---
+++ EduTrack/internal/db/db.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Init() {
+	// сначала над загрузить env наш
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Ошибка при загрузке .env файла")
+	}
+
+	// теперь читаем переменные
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	name := os.Getenv("DB_NAME")
+
+	// нужна строка подключения которую будут читать потом (dsn)
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s pass=%s name=%s", host, port, user, pass, name)
+
+	// подключаемся к базе через горм
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Не удалось подключиться к базе данных: ", err)
+	}
+
+	fmt.Println("Успешное подключение к базе данных")
+}
