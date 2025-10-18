33849855ff86365e62a7aace03545fbe2d0c6387
Sanzhar <ssoltybekov@gmail.com>
2025-10-18 22:22:43 +0500

    service teacher chi version

--- EduTrack/internal/services/teacher_service.go
+++ EduTrack/internal/services/teacher_service.go
@@ -5,40 +5,78 @@ import (
 	"edutrack/internal/models"
 )
 
-// круд учителей который в teacher.go отправлю
+// // чтение учителей который в teacher.go отправлю
 
-// через /teachers Get вызывет эту функцию чтобы все записы из бд получить
+// // через /teachers Get вызывет эту функцию чтобы все записы из бд получить
 
-func GetAllTeachers() ([]models.Teacher, error) {
+// func GetAllTeachers() ([]models.Teacher, error) {
+// 	var teachers []models.Teacher
+// 	err := db.DB.Find(&teachers).Error
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return teachers, nil
+// }
+
+// // теперь через айди находим тичеров чтобы конкретный тьютор был
+
+// func GetTeacherById(id uint) (*models.Teacher, error) {
+// 	var teacher models.Teacher
+// 	err := db.DB.First(&teacher, id).Error
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return &teacher, err
+// }
+
+// // вставляем структуру
+
+// func CreateTeacher(teacher *models.Teacher) error {
+// 	return db.DB.Create(&teacher).Error
+// }
+
+// func UpdateTeacher(teacher *models.Teacher) error {
+// 	return db.DB.Save(&teacher).Error
+// }
+
+// func DeleteTeacher(id uint) error {
+// 	return db.DB.Delete(&models.Teacher{}, id).Error
+// }
+
+// теперь то же самое на chi
+
+type TeacherService struct{}
+
+func NewTeacherService() *TeacherService {
+	return &TeacherService{}
+}
+
+func (s *TeacherService) GetAll() ([]models.Teacher, error) {
 	var teachers []models.Teacher
 	err := db.DB.Find(&teachers).Error
 	if err != nil {
 		return nil, err
 	}
-	return teachers, nil
+	return teachers, err
 }
 
-// теперь через айди находим тичеров чтобы конкретный тьютор был
-
-func GetTeacherById(id uint) (*models.Teacher, error) {
-	var teacher models.Teacher
+func (s *TeacherService) GetById(id uint) (*models.Teacher, error) {
+	var teacher *models.Teacher
 	err := db.DB.First(&teacher, id).Error
 	if err != nil {
 		return nil, err
 	}
-	return &teacher, err
+	return teacher, err
 }
 
-// вставляем структуру
-
-func CreateTeacher(teacher *models.Teacher) error {
+func (s *TeacherService) Create(teacher *models.Teacher) error {
 	return db.DB.Create(&teacher).Error
 }
 
-func UpdateTeacher(teacher *models.Teacher) error {
+func (s *TeacherService) Update(teacher *models.Teacher) error {
 	return db.DB.Save(&teacher).Error
 }
 
-func DeleteTeacher(id uint) error {
+func (s *TeacherService) Delete(id uint) error {
 	return db.DB.Delete(&models.Teacher{}, id).Error
 }
