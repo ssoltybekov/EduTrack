e4eded1934f8978bd4ceecdcd43480a32831c807
Sanzhar <ssoltybekov@gmail.com>
2025-10-19 12:57:28 +0500

    crud

--- EduTrack/go.mod
+++ EduTrack/go.mod
@@ -4,6 +4,7 @@ go 1.24.4
 
 require (
 	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.2.3
 	github.com/joho/godotenv v1.5.1
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.31.0
--- EduTrack/internal/routes/routes.go
+++ EduTrack/internal/routes/routes.go
@@ -1,1 +1,33 @@
 package routes
+
+import (
+	"edutrack/internal/handlers"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func Routes() *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Route("/teachers", func(r chi.Router) {
+		r.Get("/", handlers.ListTeacher)
+		r.Get("/{id}", handlers.GetTeacher)
+		r.Post("/", handlers.CreateTeacher)
+		r.Put("/", handlers.UpdateTeacher)
+		r.Delete("/{id}", handlers.DeleteTeacher)
+	})
+
+	r.Route("/students", func(r chi.Router) {
+		r.Get("/", handlers.ListStudents)
+		r.Get("/{id}", handlers.GetStudent)
+		r.Post("/", handlers.CreateStudent)
+		r.Put("/", handlers.UpdateStudent)
+		r.Delete("/{id}", handlers.DeleteStudent)
+	})
+
+	return r
+}
